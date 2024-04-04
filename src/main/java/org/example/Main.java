d040416850d2554fdb358fa7543b7305aa3ef610
aysenurcaglar <aysenurcaglar24@gmail.com>
2024-04-04 14:13:42 +0300

    challenge 1 done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,23 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] testStrings = {"I did, did I?", "Racecar", "hello", "Was it a car or a cat I saw?", "Ey Edip, Adana'da pide ye."};
+        for (String testStr : testStrings) {
+            System.out.println(testStr + " => " + (checkForPalindrome(testStr) ? "palindrom" : "palindrom değil"));
+        }
+    }
+
+    public static boolean checkForPalindrome(String str) {
+        String formattedStr = formatString(str);
+        String reversedStr = reverseString(formattedStr);
+        return formattedStr.equals(reversedStr);
+    }
+    private static String formatString(String str) {
+        str = str.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        return str;
+    }
+    private static String reverseString(String str) {
+        return new StringBuilder(str).reverse().toString();
     }
 
 }
